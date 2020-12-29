625a72101eea6305d0d132c08ba07b0e21d4d9be
Nima Zareian <nimazareian80@gmail.com>
2020-12-28 19:37:14 -0800

    Updated the UI design of MovieCard

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -9,13 +9,65 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
 import TrophyEmoji from "@material-ui/icons/EmojiEvents";
 import ToggleButton from "@material-ui/lab/ToggleButton";
+import Box from "@material-ui/core/Box";
+
+const cardHeight = 400;
+const cardWidth = 300;
 
 const useStyles = makeStyles({
 	root: {
 		margin: "auto",
 		maxWidth: 345,
+	},
+	card: {
+		borderRadius: "1rem",
+		boxShadow: "none",
+		position: "relative",
+		minWidth: 210,
+		minHeight: 280,
+		maxWidth: cardWidth,
+		maxHeight: cardHeight,
+		"&:after": {
+			content: '""',
+			display: "block",
+			position: "absolute",
+			width: "100%",
+			height: "64%",
+			bottom: 0,
+			zIndex: 1,
+			background: "linear-gradient(to top, #000, rgba(0,0,0,0))",
+		},
+	},
+	content: {
+		position: "absolute",
+		zIndex: 2,
+		left: 0,
+		bottom: 0,
+		width: cardWidth - 48,
+	},
+	movieTitle: {
+		color: "white",
+		// width: 200,
+		marginTop: 0,
+		marginBottom: 8,
+	},
+	movieYear: {
+		marginBottom: 0,
+	},
+	cardImg: {
+		width: cardWidth,
+		height: cardHeight,
+	},
+	nominateButton: {
+		margin: 8,
+		position: "absolute",
+		zIndex: 2,
+		color: "white",
+		top: 0,
+		right: 0,
 	},
 });
 
@@ -65,32 +117,31 @@ function MovieCard(props) {
 
 	return (
 		<div>
-			<Card className={classes.root}>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						alt={movie.Title}
-						height="517"
-						image={movie.Poster}
-						title={movie.Title}
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							{movie.Title}
-						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
-							{movie.Year}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					{/* <Button size="small" color="primary" startIcon={<TrophyEmoji />}>
-						Nominate
-					</Button> */}
-					<ToggleButton value="Nominate" selected={selected} onChange={updateNominations}>
-						<TrophyEmoji color={selected ? "secondary" : "primary"} />
-					</ToggleButton>
-				</CardActions>
+			<Card className={classes.card}>
+				{/* <CardActionArea> */}
+				<CardMedia
+					className={classes.cardImg}
+					component="img"
+					alt={movie.Title}
+					image={movie.Poster}
+					title={movie.Title}
+				/>
+				<IconButton
+					className={classes.nominateButton}
+					key="close"
+					aria-label="Close"
+					color="inherit"
+					value="Nominate"
+					selected={selected}
+					onClick={updateNominations}
+				>
+					<TrophyEmoji fontSize="large" color={selected ? "secondary" : "primary"} />
+				</IconButton>
+				<Box py={3} px={3} className={classes.content}>
+					<h2 className={classes.movieTitle}>{movie.Title}</h2>
+					<h3 className={`${classes.movieYear} ${classes.movieTitle}`}>{movie.Year}</h3>
+				</Box>
+				{/* </CardActionArea> */}
 			</Card>
 		</div>
 	);
